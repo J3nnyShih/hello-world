9394482054d31882cee3215865aaf9f552e79bbe
J3nnyShih <104306048@nccu.edu.tw>
2017-04-07 15:25:21 +0800

    Create Account.java

---
+++ Account.java
@@ -0,0 +1,15 @@
+package test;
+
+public class Account {
+	public String ID;
+	public String password;
+	public int balance;
+	public Account(String ID, String password){
+		this.ID = ID;
+		this.password = password;
+		this.balance = 0;
+	}
+	public static void main(String[] args){
+		
+	}
+}
